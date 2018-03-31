eaa68bd000b20997127240636ddf76a5debf9af8
longyi <nklongyi@qq.com>
2018-03-31 17:04:22 +0800

    了解shiro框架

---
+++ shiro01/src/main/java/myrealm01.java
@@ -0,0 +1,21 @@
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by longyi on 2018-03-30.
+ */
+public class myrealm01 implements Realm {
+    public String getName() {
+        return null;
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
